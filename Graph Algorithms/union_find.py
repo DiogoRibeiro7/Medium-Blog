87966df421e838cd038205fa8b6b65525346b179
Diogo Ribeiro <diogo_dj@hotmail.com>
2019-04-23 17:56:09 +0100

    Update union_find.py

--- Graph Algorithms/union_find.py
+++ Graph Algorithms/union_find.py
@@ -13,3 +13,29 @@ class DisjointSet(object):
     """Joins the sets containing x and y."""
     self.parent[self.find(x)] = self.find(y)
     
+    
+    
+    
+import unittest
+class DisjointSetTest(unittest.TestCase):
+  def test_initialized_state(self):
+    d = DisjointSet(3)
+    self.assertEqual(d.find(0), 0)
+    self.assertEqual(d.find(1), 1)
+    self.assertEqual(d.find(2), 2)
+  def test_basic_union(self):
+    d = DisjointSet(3)
+    d.union(0, 1)
+    self.assertEqual(d.find(0), d.find(1))
+    self.assertNotEqual(d.find(1), d.find(2))
+  def test_basic_union_idempotent(self):
+    d = DisjointSet(2)
+    d.union(0, 1)
+    d.union(0, 1)
+    self.assertEqual(d.find(0), d.find(1))
+  def test_union_all(self):
+    d = DisjointSet(100)
+    for i in range(1, 100):
+      d.union(i - 1, i)
+    for i in range(1, 100):
+      self.assertEqual(d.find(0), d.find(i))
