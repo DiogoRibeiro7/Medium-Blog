ba3b3b59711feaa8b4e431d6efb26b49e3541fef
DiogoRibeiro7 <diogo_dj@hotmail.com>
2023-07-10 10:36:07 +0100

    feat: survival analysis

--- survival_analysis/survival_analysis_tesco.py
+++ survival_analysis/survival_analysis_tesco.py
@@ -10,7 +10,7 @@ from lifelines.utils import qth_survival_times
 # Read the data
 df = pd.read_csv('Telco-Customer-Churn.csv')
 
-# Convert TotalCharges to numeric (there are some blank strings which are not valid float)
+# Convert TotalCharges to numeric
 df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
 
 # Drop any rows with missing data
@@ -27,7 +27,7 @@ df['Contract'] = df['Contract'].apply(lambda x: 1 if x == 'One year' else 0 if x
 cph = CoxPHFitter()
 cph.fit(df[['tenure', 'MonthlyCharges', 'TotalCharges', 'gender', 'Contract', 'Churn']], 'tenure', event_col='Churn')
 
-# Create a survival function for a new customer with a monthly bill of $70, is male, and has a one year contract
+# Create a survival function for a new customer
 new_customer = pd.DataFrame.from_dict({
     'tenure': [0],
     'MonthlyCharges': [70],
@@ -42,9 +42,9 @@ new_customer_survival_function = cph.predict_survival_function(new_customer)
 
 # Calculate CLV for different percentile survival times (e.g., 25th, 50th, 75th percentiles)
 percentiles = [0.25, 0.5, 0.75]
-survival_times = qth_survival_times(percentiles, new_customer_survival_function)
 clv = {}
-for i, time in enumerate(survival_times):
-    clv[f'{int(percentiles[i]*100)}_percentile'] = time[0] * new_customer['MonthlyCharges'][0]
+for p in percentiles:
+    survival_time = qth_survival_times(p, new_customer_survival_function)
+    clv[f'{int(p*100)}_percentile'] = survival_time * new_customer['MonthlyCharges'].values[0]
     
 print(f'The CLV of the new customer at different percentiles is {clv}')
