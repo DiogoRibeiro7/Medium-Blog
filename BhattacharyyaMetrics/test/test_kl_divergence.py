4d877f60779b1b52eda334602b6051be991d99ed
DiogoRibeiro7 <diogo_dj@hotmail.com>
2023-08-17 22:54:22 +0100

    chore: bhattacharyya_metrics

---
+++ BhattacharyyaMetrics/test/test_kl_divergence.py
@@ -0,0 +1,11 @@
+from bhattacharyya_metrics import KLDivergence
+
+def test_kl_divergence():
+    p = [0.4, 0.3, 0.2, 0.1]
+    q = [0.3, 0.3, 0.2, 0.2]
+
+    kl_calculator = KLDivergence(p, q)
+    kl_divergence = kl_calculator.kl_divergence()
+
+    # Add your assertion here based on expected value
+    assert np.isclose(kl_divergence, expected_value, atol=1e-6)
