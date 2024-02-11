d0703ce7319ba5d809f4576408b9a0280482f46b
DiogoRibeiro7 <diogo_dj@hotmail.com>
2024-02-11 19:05:51 +0000

    chore: code

---
+++ R_code/EDA.R
@@ -0,0 +1,14 @@
+# Load necessary libraries
+library(ISLR) # For the dataset
+library(ggplot2) # For data visualization
+library(dplyr) # For data manipulation
+
+# Load the Carseats dataset
+data("Carseats")
+
+# Explore the dataset
+dim(Carseats) # Dimensions of the dataset
+str(Carseats) # Structure of the dataset
+summary(Carseats) # Summary statistics
+
+
